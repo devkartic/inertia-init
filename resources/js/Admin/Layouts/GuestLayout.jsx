289da4ff44c12c6c89af7eb7eb79826f9f2de71d
dev.kartic <kartic.gharami@gmail.com>
2023-09-29 00:20:25 +0600

    Working with login form

--- resources/js/Admin/Layouts/GuestLayout.jsx
+++ resources/js/Admin/Layouts/GuestLayout.jsx
@@ -8,7 +8,7 @@ export default function GuestL({ children }) {
                 <div className="row justify-content-center py-3 py-md-5">
                     <div className="col-md-4 my-3 pb-5 rounded">
                         <div className="h2 text-center my-3"><span className="text-primary">E-Shop</span> Login</div>
-                        <div className="card shadow border-0 p-5">
+                        <div className="card shadow border-0 p-md-5">
                             <div className="card-body">
                                 {children}
                             </div>
