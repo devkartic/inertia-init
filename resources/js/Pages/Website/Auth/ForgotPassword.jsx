cd7523e4798fe653bd94a875aa21922f654f5227
dev.kartic <kartic.gharami@gmail.com>
2023-09-29 22:37:43 +0600

    Working with forgot password page

--- resources/js/Pages/Website/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Website/Auth/ForgotPassword.jsx
@@ -1,8 +1,9 @@
-import GuestLayout from '@/Layouts/GuestLayout';
+import GuestLayout from '@/Admin/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
 import { Head, useForm } from '@inertiajs/react';
+import InputLabel from "@/Components/InputLabel.jsx";
 
 export default function ForgotPassword({ status }) {
     const { data, setData, post, processing, errors } = useForm({
@@ -19,28 +20,22 @@ export default function ForgotPassword({ status }) {
         <GuestLayout>
             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600">
+            <div className="my-3 text-dark-emphasis">
                 Forgot your password? No problem. Just let us know your email address and we will email you a password
                 reset link that will allow you to choose a new one.
             </div>
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && <div className="alert alert-warning" role="alert">{status}</div>}
 
             <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
+                <div className="form-floating mb-3">
+                    <TextInput type="email" name="email" className="form-control" id="floatingEmail" autoComplete="email" isFocused={true} value={data.email} onChange={(e) => setData('email', e.target.value)}/>
+                    <InputLabel htmlFor="floatingEmail">Email</InputLabel>
+                    <InputError className="form-text text-danger" message={errors.email}/>
+                </div>
 
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="flex items-center justify-end mt-4">
-                    <PrimaryButton className="ml-4" disabled={processing}>
+                <div className="d-grid">
+                    <PrimaryButton className="btn btn-primary" disabled={processing}>
                         Email Password Reset Link
                     </PrimaryButton>
                 </div>
