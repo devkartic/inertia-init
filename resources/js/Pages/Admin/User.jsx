02bcae24be346749a7656f21b31e10f4528ba0fb
dev.kartic <kartic.gharami@gmail.com>
2023-10-05 22:31:19 +0600

    Working with User list view and controller data interact with inertia

--- resources/js/Pages/Admin/User.jsx
+++ resources/js/Pages/Admin/User.jsx
@@ -2,8 +2,9 @@ import AuthenticatedLayout from '@/Admin/Layouts/AuthenticatedLayout.jsx';
 import { Head } from '@inertiajs/react';
 import {Button, Table} from "react-bootstrap";
 import {FaEye, FaEdit, FaTrash} from "react-icons/fa";
-export default function Dashboard({ auth, props }) {
-    console.log(props);
+import moment from 'moment';
+export default function Users({ auth, users }) {
+    console.log(users);
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -24,48 +25,24 @@ export default function Dashboard({ auth, props }) {
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <>
-                                <Button variant="outline-info"><FaEye/></Button>{' '}
-                                <Button variant="outline-warning"><FaEdit/></Button>{' '}
-                                <Button variant="outline-danger"><FaTrash/></Button>
-                            </>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <>
-                                <Button variant="outline-info"><FaEye/></Button>{' '}
-                                <Button variant="outline-warning"><FaEdit/></Button>{' '}
-                                <Button variant="outline-danger"><FaTrash/></Button>
-                            </>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>
-                            <>
-                                <Button variant="outline-info"><FaEye/></Button>{' '}
-                                <Button variant="outline-warning"><FaEdit/></Button>{' '}
-                                <Button variant="outline-danger"><FaTrash/></Button>
-                            </>
-                        </td>
-                    </tr>
+                    {users.map((user, index)=>{
+                        return (
+                            <tr key={index}>
+                                <td>{++index}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{moment(user.created_at).format('LL, hh:mm:ss A')}</td>
+                                <td>{moment(user.updated_at).format('LL, hh:mm:ss A')}</td>
+                                <td>
+                                    <>
+                                        <Button variant="outline-info"><FaEye/></Button>{' '}
+                                        <Button variant="outline-warning"><FaEdit/></Button>{' '}
+                                        <Button variant="outline-danger"><FaTrash/></Button>
+                                    </>
+                                </td>
+                            </tr>
+                        );
+                    })}
                     </tbody>
                 </Table>
             </div>
